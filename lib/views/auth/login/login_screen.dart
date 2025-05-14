4f997f63120d14ee08eee9f60e46bfb945dbed34
Nilesh <nileshtiwari@itgeeks.com>
2025-05-14 18:22:57 +0530

    ui changes

--- lib/views/auth/login/login_screen.dart
+++ lib/views/auth/login/login_screen.dart
@@ -66,157 +66,221 @@ class _LoginScreenState extends State<LoginScreen> {
         child: BlocBuilder<LoginScreenBloc, LoginScreenState>(
             builder: (context, state) {
           return Scaffold(
+            backgroundColor: Color.fromARGB(255, 9, 1, 64),
             // resizeToAvoidBottomInset: false,
-            body: SingleChildScrollView(
-              physics: ClampingScrollPhysics(),
-              child: Container(
-                padding:
-                    EdgeInsets.only(left: 20, right: 20, top: 35, bottom: 30),
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height,
-                // color: AppTheme.backgroundColor!, //.white70,
-                child: Column(
-                  children: <Widget>[
-                    Flexible(
-                      flex: 1,
-                      child: InkWell(
-                        child: Container(
-                          child: Align(
-                            alignment: Alignment.topLeft,
-                            child: Icon(Icons.close),
+            body: Stack(
+              children: [
+                ClipPath(
+                  clipper: SlantedWaveClipper(),
+                  child: Container(
+                      height: MediaQuery.of(context).size.height,
+                      color: const Color.fromARGB(255, 167, 167,
+                          167) //const Color(0xFF014034), // Deep green
+                      ),
+                ),
+                SingleChildScrollView(
+                  physics: ClampingScrollPhysics(),
+                  child: Container(
+                    padding: EdgeInsets.only(
+                        left: 20, right: 20, top: 35, bottom: 30),
+                    width: double.infinity,
+                    height: MediaQuery.of(context).size.height,
+                    // color: AppTheme.backgroundColor!, //.white70,
+                    child: Column(
+                      children: <Widget>[
+                        Flexible(
+                          flex: 1,
+                          child: InkWell(
+                            child: Container(
+                              child: Align(
+                                alignment: Alignment.topLeft,
+                                child: Icon(Icons.close),
+                              ),
+                            ),
+                            onTap: () {
+                              Navigator.pop(context);
+                            },
                           ),
                         ),
-                        onTap: () {
-                          Navigator.pop(context);
-                        },
-                      ),
-                    ),
-                    Flexible(
-                      flex: 5,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Container(
-                              width: 130,
-                              height: 130,
-                              alignment: Alignment.center,
-                              child: Text("Login")
-                              // Image.asset("assets/images/ic_app_icon.png"),
-                              ),
-                          GapWidget(),
-                          CustomTextFormField()
-                              .SimpleTextFormFieldWithPrefixIcon(
-                            hintText: "Email",
-                            prefixIcon: "assets/svg/Email.svg",
-                            context: context,
-                            controller: emailController,
-                            keyboardType: TextInputType.emailAddress,
-                          ),
-                          GapWidget(),
-                          SimpleTextFormFieldWithPrefixIconAndVisibilityToggle(
-                            context: context,
-                            controller: passwordController,
-                            hintText: "Password",
-                            prefixIcon: "assets/svg/lock_pass.svg",
-                            obscureText: true,
-                          ),
-                          GapWidget(),
-                          Row(
+                        Flexible(
+                          flex: 5,
+                          child: Column(
                             mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              LinkButton(
-                                text: "Forgot your password?",
-                                onPressed: () {},
+                            children: <Widget>[
+                              // Container(
+                              //     width: 130,
+                              //     height: 130,
+                              //     alignment: Alignment.center,
+                              //     child: Text("Login")
+                              //     // Image.asset("assets/images/ic_app_icon.png"),
+                              //     ),
+                              GapWidget(),
+                              CustomTextFormField()
+                                  .SimpleTextFormFieldWithPrefixIcon(
+                                hintText: "Email",
+                                prefixIcon: "assets/svg/Email.svg",
+                                context: context,
+                                controller: emailController,
+                                keyboardType: TextInputType.emailAddress,
+                              ),
+                              GapWidget(),
+                              SimpleTextFormFieldWithPrefixIconAndVisibilityToggle(
+                                context: context,
+                                controller: passwordController,
+                                hintText: "Password",
+                                prefixIcon: "assets/svg/lock_pass.svg",
+                                obscureText: true,
+                              ),
+                              GapWidget(),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  LinkButton(
+                                    text: "Forgot your password?",
+                                    onPressed: () {},
+                                  )
+                                ],
+                              ),
+                              GapWidget(),
+                              state is LoginScreenSubmittingState
+                                  ? CustomDialogButton()
+                                  : CustomButton(
+                                      text: 'Sign In',
+                                      onPressed: () {
+                                        if (emailController.text.isEmpty) {
+                                          Dialogs.errorDialog(
+                                              message: 'Email is Required');
+                                        } else if (passwordController
+                                            .text.isEmpty) {
+                                          Dialogs.errorDialog(
+                                              message: 'passwordRequired');
+                                        } else if (!Validations.emailValidation(
+                                            emailController.text)) {
+                                          Dialogs.errorDialog(
+                                              message: 'Email is Invalid');
+                                        } else if (passwordController.text
+                                            .trim()
+                                            .isEmpty) {
+                                          Dialogs.errorDialog(
+                                              message: 'Password is Required');
+                                        } else {
+                                          context.read<LoginScreenBloc>().add(
+                                              onLoginScreenButtonClick(
+                                                  emailController.text.trim(),
+                                                  passwordController.text
+                                                      .trim()));
+                                        }
+                                      },
+                                    ),
+                              GapWidget(
+                                size: -5,
                               )
                             ],
                           ),
-                          GapWidget(),
-                          state is LoginScreenSubmittingState
-                              ? CustomDialogButton()
-                              : CustomButton(
-                                  text: 'Sign In',
-                                  onPressed: () {
-                                    if (emailController.text.isEmpty) {
-                                      Dialogs.errorDialog(
-                                          message: 'Email is Required');
-                                    } else if (passwordController
-                                        .text.isEmpty) {
-                                      Dialogs.errorDialog(
-                                          message: 'passwordRequired');
-                                    } else if (!Validations.emailValidation(
-                                        emailController.text)) {
-                                      Dialogs.errorDialog(
-                                          message: 'Email is Invalid');
-                                    } else if (passwordController.text
-                                        .trim()
-                                        .isEmpty) {
-                                      Dialogs.errorDialog(
-                                          message: 'Password is Required');
-                                    } else {
-                                      context.read<LoginScreenBloc>().add(
-                                          onLoginScreenButtonClick(
-                                              emailController.text.trim(),
-                                              passwordController.text.trim()));
-                                    }
-                                  },
-                                ),
-                          GapWidget(
-                            size: -5,
-                          )
-                        ],
-                      ),
-                    ),
-                    Flexible(
-                      flex: 1,
-                      child: Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Container(
-                              child: Text(
-                                "Don't have an account? ",
-                                style: TextStyle(
-                                  color: Color(0xFF666666),
-                                  fontFamily: defaultFontFamily,
-                                  fontSize: defaultFontSize,
-                                  fontStyle: FontStyle.normal,
-                                ),
-                              ),
-                            ),
-                            LinkButton(
-                                text: "Sign Up",
-                                onPressed: () {
-                                  // if (goBack) {
-                                  //   SignUpScreen signUpScreen =
-                                  //       SignUpScreen(goBack);
-                                  //   bool? refresh = await context.push(
-                                  //       "/${Routes.signUpScreen}",
-                                  //       extra: signUpScreen);
-                                  //   if (refresh != null) {
-                                  //     if (refresh as bool) {
-                                  //       // context.pop(true);
-                                  //       Navigator.pop(context);
-                                  //     }
-                                  //   }
-                                  // } else {
-                                  //extra goback
-                                  Navigator.of(context).push(MaterialPageRoute(
-                                      builder: (context) => SignUpScreen()));
-                                  // }
-                                }),
-                          ],
                         ),
-                      ),
-                    )
-                  ],
+                        Flexible(
+                          flex: 1,
+                          child: Align(
+                            alignment: Alignment.bottomCenter,
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Container(
+                                  child: Text(
+                                    "Don't have an account? ",
+                                    style: TextStyle(
+                                      color: Color(0xFF666666),
+                                      fontFamily: defaultFontFamily,
+                                      fontSize: defaultFontSize,
+                                      fontStyle: FontStyle.normal,
+                                    ),
+                                  ),
+                                ),
+                                LinkButton(
+                                    text: "Sign Up",
+                                    onPressed: () {
+                                      // if (goBack) {
+                                      //   SignUpScreen signUpScreen =
+                                      //       SignUpScreen(goBack);
+                                      //   bool? refresh = await context.push(
+                                      //       "/${Routes.signUpScreen}",
+                                      //       extra: signUpScreen);
+                                      //   if (refresh != null) {
+                                      //     if (refresh as bool) {
+                                      //       // context.pop(true);
+                                      //       Navigator.pop(context);
+                                      //     }
+                                      //   }
+                                      // } else {
+                                      //extra goback
+                                      Navigator.of(context).push(
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  SignUpScreen()));
+                                      // }
+                                    }),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
           );
         }),
       ),
     );
   }
+}
+
+class SlantedWaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    // // Step 1: Start top-left and go down to the bottom-left corner (with offset)
+    // path.lineTo(0, size.height - 60);
+
+    // // Step 2: Curve downward from left to center
+    // path.quadraticBezierTo(
+    //   size.width * 0.25, size.height,       // control point
+    //   size.width * 0.5, size.height - 30,   // end point
+    // );
+
+    // // Step 3: Curve upward from center to right
+    // path.quadraticBezierTo(
+    //   size.width * 0.75, size.height - 60,  // control point
+    //   size.width, size.height - 20,         // end point
+    // );
+
+    // // Step 4: Line up to top-right
+    // path.lineTo(size.width, 0);
+
+    // // Step 1: Start top-left and go down to the bottom-left corner (with offset)
+    // path.lineTo(0, size.height);
+    // path.lineTo(size.width * 0.40, size.height - (size.height / 1.8));
+    // path.lineTo(size.width * 0.60, size.height - (size.height / 2.4));
+    // path.lineTo(size.width, 0);
+
+    // Step 1: Start top-left and go down to the bottom-left corner (with offset)
+    path.lineTo(0, size.height);
+    path.lineTo(size.width * 0.45, size.height - (size.height / 2.15));
+    path.lineTo(size.width * 0.45, size.height - (size.height / 1.85));
+    path.lineTo(size.width * 0.55, size.height - (size.height / 2.15));
+    path.lineTo(size.width * 0.55, size.height - (size.height / 1.85));
+
+    // Step 4: Line up to top-right
+    path.lineTo(size.width, 0);
+
+    // Step 5: Close the path (draw back to start)
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
 }
